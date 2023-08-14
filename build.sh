a6826c0a9b26a8e6e56fbfc61a69224187b41750
andrey4d <andrey4d.dev@gmail.com>
2023-08-15 00:36:08 +0700

    FIX PORT specification

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker build  --platform linux/arm64 --network host -t ${IMAGE_NAME}:${VERSION}-
 docker build  --platform linux/amd64 --network host -t ${IMAGE_NAME}:${VERSION}-amd64 build
 
 docker push ${IMAGE_NAME}:${VERSION}-arm64 
-docker push  ${IMAGE_NAME}:${VERSION}-amd64
+docker push ${IMAGE_NAME}:${VERSION}-amd64
 
 docker manifest create ${IMAGE_NAME}:${VERSION} --amend ${IMAGE_NAME}:${VERSION}-arm64 --amend ${IMAGE_NAME}:${VERSION}-amd64
 docker manifest push ${IMAGE_NAME}:${VERSION}
